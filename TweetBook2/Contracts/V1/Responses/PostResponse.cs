ad025c55bd90c1267b9ec6902620c8c531f1cb02
NgoNhatDuy <ngonhatduy240297@gmail.com>
2022-01-20 21:31:14 +0700

    Add post request

---
+++ TweetBook2/Contracts/V1/Responses/PostResponse.cs
@@ -0,0 +1,7 @@
+﻿namespace TweetBook2.Contracts.V1.Responses
+{
+    public class PostResponse
+    {
+        public string Id { get; set; }
+    }
+}
--- TweetBook2/Controllers/V1/PostsController.cs
+++ TweetBook2/Controllers/V1/PostsController.cs
@@ -2,6 +2,8 @@
 using System;
 using System.Collections.Generic;
 using TweetBook2.Contracts.V1;
+using TweetBook2.Contracts.V1.Requests;
+using TweetBook2.Contracts.V1.Responses;
 using TweetBook2.Domain;
 
 namespace TweetBook2.Controllers.V1
@@ -23,6 +25,22 @@ namespace TweetBook2.Controllers.V1
         public IActionResult GetAll()
         {
             return Ok(_posts);
+        }
+
+        [HttpPost(ApiRoutes.Posts.Create)]
+        public IActionResult Create([FromBody] CreatePostRequest postRequest)
+        {
+            var post = new Post { Id = postRequest.Id };
+
+            if(string.IsNullOrEmpty(post.Id))
+                post.Id = Guid.NewGuid().ToString();
+
+            _posts.Add(post);
+
+            var baseUrl = $"{HttpContext.Request.Scheme}://{HttpContext.Request.Host}/";
+            var locationUrl = baseUrl + ApiRoutes.Posts.Get.Replace("{postId}", post.Id);
+            var response = new PostResponse { Id = post.Id };
+            return Created(locationUrl, response);
         }
     }
 }
