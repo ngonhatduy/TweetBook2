8ef0ebe24a62b15f70fe80ea6d652c88e9b5cce4
NgoNhatDuy <ngonhatduy240297@gmail.com>
2022-02-17 15:29:51 +0700

    Change config

--- TweetBook2/Controllers/V1/PostsController.cs
+++ TweetBook2/Controllers/V1/PostsController.cs
@@ -21,7 +21,7 @@ namespace TweetBook2.Controllers.V1
             _postService = postService;
         }
 
-        [Authorize(Roles = "Admin")]
+        //[Authorize(Roles = "Admin")]
         [HttpGet(ApiRoutes.Posts.GetAll)]
         public async Task<IActionResult> GetAll()
         {
